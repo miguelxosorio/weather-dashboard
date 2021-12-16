99e84a90532d3311615e3f148ad5edb52b6a0745
vayne <miguel.osorio19@yahoo.com>
2021-12-16 15:17:18 -0600

    syncing the <input> and <button> function, console logging the value in <input>

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,27 +14,58 @@ console.log(today); // console logging how the date appears
 // date looks like "Wednesday, December 15, 2021 2:58 PM"
 
 // Declaring variables to target HTML elements
-let searchBoxEl = document.getElementById('enter-city');
-let searchBtnEl = document.getElementById('search-btn');
+// let searchBoxEl = document.getElementById('enter-city');
+// let searchBtnEl = document.getElementById('search-btn');
 
-var init = function() {
+// var init = function() {
 
+//     fetch(queryURL)
+//     .then(response => response.json())
+//     .then(data => console.log(data));
+
+//     document.getElementById('date').innerHTML = "Need Dynamic Date";
+//     document.getElementById('temperature').innerHTML = "Temperature";
+//     document.getElementById('humidity').innerHTML = "Humidity";
+//     document.getElementById('windspeed').innerHTML = "Wind Speed";
+//     document.getElementById('uv').innerHTML = "UV-Index";
+
+//     document.getElementById('card1').innerHTML = "Needs Dynamic Forecast";
+//     document.getElementById('card2').innerHTML = "Needs Dynamic Forecast";
+//     document.getElementById('card3').innerHTML = "Needs Dynamic Forecast";
+//     document.getElementById('card4').innerHTML = "Needs Dynamic Forecast";
+//     document.getElementById('card5').innerHTML = "Needs Dynamic Forecast";
+
+    
+
+
+// };
+// searchBtnEl.addEventListener("click", init);
+
+
+// button.addeventListener("click", function(event){
+//      event.preventDefault();
+//      let inputValue = document.querySelector("#idofinput").value
+//      inputValue will be the value of the users input
+//      then you would fetch with that data
+//      other logic
+// })
+
+// Capturing the value of the <input> after clicking the search button
+// declaring the button element as a variable
+var searchBtnEl = document.getElementById('search-btn');
+// Creating the function for the click
+searchBtnEl.addEventListener("click", function(event){
+    event.preventDefault();
+
+    // creating a variable for the <input> and putting value in the entry
+    let city = document.getElementById('enter-city').value;
+    console.log(city);
+
+    // fetching API
     fetch(queryURL)
-    .then(response => response.json())
-    .then(data => console.log(data));
+     .then(response => response.json())
+     .then(data => console.log(data));
 
-    document.getElementById('date').innerHTML = "Need Dynamic Date";
-    document.getElementById('temperature').innerHTML = "Temperature";
-    document.getElementById('humidity').innerHTML = "Humidity";
-    document.getElementById('windspeed').innerHTML = "Wind Speed";
-    document.getElementById('uv').innerHTML = "UV-Index";
 
-    document.getElementById('card1').innerHTML = "Needs Dynamic Forecast";
-    document.getElementById('card2').innerHTML = "Needs Dynamic Forecast";
-    document.getElementById('card3').innerHTML = "Needs Dynamic Forecast";
-    document.getElementById('card4').innerHTML = "Needs Dynamic Forecast";
-    document.getElementById('card5').innerHTML = "Needs Dynamic Forecast";
-
-};
-searchBtnEl.addEventListener("click", init);
+})
 
